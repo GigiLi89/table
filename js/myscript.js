a55e606a71685b2324a5958e8eabda1c0d206d7c
GigiLi89 <li.gigi89@gmail.com>
2024-04-15 09:37:18 +0000

    jQuery, changing teble color etc explained

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+   $("h2").addClass("underline"); //underlines all <h2> elements
+   $("ul").addClass("border"); //adds a border to all lists
+});
+
+// Using the removeClass instead of the addClass will remove the class from the element
